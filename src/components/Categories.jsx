5182a149c95aa0d4cfba2f76ffe5ecccf7aaab6c
zionjke <vincere802@gmail.com>
2020-07-15 23:26:30 +0300

    Сделал компоненту Categories, повесил active на обработчик.

---
+++ src/components/Categories.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from "react";
+
+const Categories = ({items,onClickItem}) => {
+    const [activeItem, setActiveItem] = useState(null);
+    const onSelectItem = index => {
+        setActiveItem(index)
+    };
+    return (
+        <div className="categories">
+            <ul>
+                <li onClick={()=> onSelectItem(null)}
+                    className={`${activeItem === null && 'active'}`}
+                >
+                    Все
+                </li>
+                {   items &&
+                    items.map((item,index) =>
+                    <li className={`${activeItem === index && 'active'}`}
+                        onClick={() => onSelectItem(index)}
+                        key={`${item}_${index}`}
+                    >
+                        {item}
+                    </li>
+                )
+                }
+            </ul>
+        </div>
+    )
+};
+
+export default Categories
