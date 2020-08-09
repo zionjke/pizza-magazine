da3475e492ff90cff0eec4177a174fbbfa0d419a
zionjke <vincere802@gmail.com>
2020-08-09 11:37:26 +0300

    Updates

---
+++ precache-manifest.2497b8a3eb7954a1dbf82c3ecabbd7dd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d733240375a2253825374202ab7edbef",
+    "url": "/pizza-magazine/index.html"
+  },
+  {
+    "revision": "5cf54651c5e8ad21247e",
+    "url": "/pizza-magazine/static/css/main.e8385819.chunk.css"
+  },
+  {
+    "revision": "8fcb847e028aa94df116",
+    "url": "/pizza-magazine/static/js/2.eeb2cb84.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pizza-magazine/static/js/2.eeb2cb84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf54651c5e8ad21247e",
+    "url": "/pizza-magazine/static/js/main.03027616.chunk.js"
+  },
+  {
+    "revision": "0c1d86361c92bd766195",
+    "url": "/pizza-magazine/static/js/runtime-main.9349b8b5.js"
+  },
+  {
+    "revision": "4325ed848b66ed370420ea7fea09bc80",
+    "url": "/pizza-magazine/static/media/ProximaNova-Black.4325ed84.eot"
+  },
+  {
+    "revision": "a0606c5fef28b9e57f501db027063141",
+    "url": "/pizza-magazine/static/media/ProximaNova-Black.a0606c5f.ttf"
+  },
+  {
+    "revision": "b978d1b46a86df7a012cf8442936a1cb",
+    "url": "/pizza-magazine/static/media/ProximaNova-Black.b978d1b4.woff"
+  },
+  {
+    "revision": "10141a9d737da9c84e6e14ec5a562c6c",
+    "url": "/pizza-magazine/static/media/ProximaNova-Bold.10141a9d.eot"
+  },
+  {
+    "revision": "926a08fb27e3303c7452b0bdd2d5e5ab",
+    "url": "/pizza-magazine/static/media/ProximaNova-Bold.926a08fb.ttf"
+  },
+  {
+    "revision": "e2cf3dc2f079bf3d5185a02552f153c4",
+    "url": "/pizza-magazine/static/media/ProximaNova-Bold.e2cf3dc2.woff"
+  },
+  {
+    "revision": "169bc0f1eb2d8d0d8678014d368b0226",
+    "url": "/pizza-magazine/static/media/ProximaNova-Extrabld.169bc0f1.eot"
+  },
+  {
+    "revision": "51563919fd1af7376fac14b1194a8e4e",
+    "url": "/pizza-magazine/static/media/ProximaNova-Extrabld.51563919.ttf"
+  },
+  {
+    "revision": "ccf59c77109aecc8deeeede82e3bac87",
+    "url": "/pizza-magazine/static/media/ProximaNova-Extrabld.ccf59c77.woff"
+  },
+  {
+    "revision": "2d2ae2556b24a45ff8d5ed86b07b5783",
+    "url": "/pizza-magazine/static/media/ProximaNova-Regular.2d2ae255.woff"
+  },
+  {
+    "revision": "7ce6760d17685c466ba04d1b2c63c38b",
+    "url": "/pizza-magazine/static/media/ProximaNova-Regular.7ce6760d.ttf"
+  },
+  {
+    "revision": "b7c512788e3c77b0196f0bace8a88418",
+    "url": "/pizza-magazine/static/media/ProximaNova-Regular.b7c51278.eot"
+  },
+  {
+    "revision": "ceb2faf089a96bede51ccde3693d012b",
+    "url": "/pizza-magazine/static/media/empty-cart.ceb2faf0.png"
+  },
+  {
+    "revision": "f3327011e80a4caf08837bd844e21996",
+    "url": "/pizza-magazine/static/media/pizza-logo.f3327011.svg"
+  }
+]);
